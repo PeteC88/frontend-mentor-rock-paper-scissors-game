964cefefcc9f614d7c50c325351f412a90b46848
PeteC88 <40523064+PeteC88@users.noreply.github.com>
2022-01-20 00:02:59 +0100

    Add files via upload
    added test script for flip animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log("test");
+let paperNode = document.querySelector(".app__paper");
+let appContainer = document.querySelector(".app__container")
+
+paperNode.addEventListener("click", () => {
+    appContainer.classList.add("app__flip--active");
+})
